cc87271c3e84c19c488b108fc84794896d25f697
getrajer533 <getrajer533@gmail.com>
2020-11-03 23:49:39 +0000

    more

---
+++ 1251A-BrokenKeyboard/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1251A_BrokenKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] results = new string[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string text = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ 560A-CurrencySystemInGeraldion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _560A_CurrencySystemInGeraldion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            string[] numbers = Console.ReadLine().Split();
+
+            int[] arrayNum = new int[m];
+
+            for(int i = 0; i < m; i++)
+            {
+                int n = Convert.ToInt32(numbers[i]);
+
+                if(n == 1)
+                {
+                    Console.WriteLine(-1);
+                    System.Environment.Exit(0);
+                }
+            }
+
+            Console.WriteLine(1);
+
+        }
+    }
+}
