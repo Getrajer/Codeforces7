ba8a0aa9233b975118cdc8772c7c363bd1256cb2
getrajer533 <getrajer533@gmail.com>
2020-11-04 20:57:55 +0000

    m

--- 1251A-BrokenKeyboard/Program.cs
+++ 1251A-BrokenKeyboard/Program.cs
@@ -13,6 +13,53 @@ namespace _1251A_BrokenKeyboard
             for(int i = 0; i < n; i++)
             {
                 string text = Console.ReadLine();
+
+                if(text.Length == 1)
+                {
+                    results[i] = text;
+                }
+                else
+                {
+                    for(int j = 0; j < text.Length; j++)
+                    {
+                        if(j == 0)
+                        {
+
+                        }
+                        else
+                        {
+                            if(text == "")
+                            {
+                                break;
+                            }
+
+                            if(text[j-1] == text[j])
+                            {
+                                text = text.Remove(j - 1, 2);
+                                if(text != "")
+                                {
+                                    j -= 2;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                                
+                            }
+                            else
+                            {
+                            }
+                        }
+                    }
+
+                    results[i] = text;
+                }
+            }
+
+
+            for(int i = 0; i < results.Length; i++)
+            {
+                Console.WriteLine(results[i]);
             }
 
         }
